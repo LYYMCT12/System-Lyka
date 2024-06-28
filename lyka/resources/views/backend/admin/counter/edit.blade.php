b12dd680a703f6f618dd805580901f8724b533bf
LYYMCT12 <159540985+LYYMCT12@users.noreply.github.com>
2024-06-28 21:10:24 +0800

    added bunch of changes

--- lyka/resources/views/backend/admin/counter/edit.blade.php
+++ lyka/resources/views/backend/admin/counter/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.backend')
 @section('title', trans('app.update_counter'))
- 
+
 @section('content')
 <div class="panel panel-primary">
 
@@ -10,7 +10,7 @@
                 <h3>{{ trans('app.update_counter') }}</h3>
             </div>
             <div class="col-sm-4 text-right">
-                <button type="button" onclick="printContent('PrintMe')" class="btn btn-info" ><i class="fa fa-print"></i></button> 
+                <button type="button" onclick="printContent('PrintMe')" class="btn btn-info" ><i class="fa fa-print"></i></button>
             </div>
         </div>
     </div>
@@ -19,7 +19,7 @@
         {{ Form::open(['url' => 'admin/counter/edit', 'class'=>'col-md-7 col-sm-8']) }}
 
             <input type="hidden" name="id" value="{{ $counter->id }}">
-     
+
             <div class="form-group @error('name') has-error @enderror">
                 <label for="name">{{ trans('app.name') }} <i class="text-danger">*</i></label>
                 <input type="text" name="name" id="name" class="form-control" placeholder="{{ trans('app.name') }}" value="{{ old('name')?old('name'):$counter->name }}">
@@ -27,33 +27,33 @@
             </div>
 
             <div class="form-group @error('description') has-error @enderror">
-                <label for="description">{{ trans('app.description') }} </label> 
+                <label for="description">{{ trans('app.description') }} </label>
                 <textarea name="description" id="description" class="form-control" placeholder="{{ trans('app.description') }}">{{ old('description')?old('description'):$counter->description }}</textarea>
                 <span class="help-block text-danger">{{ $errors->first('description') }}</span>
             </div>
 
             <div class="form-group @error('status') has-error @enderror">
                 <label for="status">{{ trans('app.status') }} <i class="text-danger">*</i></label>
-                <div id="status"> 
+                <div id="status">
                     <label class="radio-inline">
                         <input type="radio" name="status" value="1" {{ ((old('status') || $counter->status)==1)?"checked":"" }}> {{ trans('app.active') }}
                     </label>
                     <label class="radio-inline">
                         <input type="radio" name="status" value="0" {{ ((old('status') || $counter->status)==0)?"checked":"" }}> {{ trans('app.deactive') }}
-                    </label> 
+                    </label>
                 </div>
-            </div>  
+            </div>
 
             <div class="form-group">
                 <button class="button btn btn-info" type="reset"><span>{{ trans('app.reset') }}</span></button>
-                <button class="button btn btn-success" type="submit"><span>{{ trans('app.update') }}</span></button> 
+                <button class="button btn btn-success" type="submit"><span>{{ trans('app.update') }}</span></button>
             </div>
-        
-        {{ Form::close() }} 
-    </div> 
-</div>  
+
+        {{ Form::close() }}
+    </div>
+</div>
 @endsection
 
 
- 
+
 
